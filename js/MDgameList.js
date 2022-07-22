35530e284c8ad6c6327b780163fe7ab5f5f11381
twn1252839 <107757895+twn1252839@users.noreply.github.com>
2022-07-22 23:54:16 +0800

    add1

---
+++ js/MDgameList.js
@@ -0,0 +1,138 @@
+$(".MDgameList1 img").click(function () {
+  $(this).attr("src", "./img/桌機index_img/GAME/MDgameList/List1.png");
+  $(".MDgameList2 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/2.png"
+  );
+  $(".MDgameList3 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/3.png"
+  );
+  $(".MDgameList4 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/4.png"
+  );
+  $(".MDgameList5 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/5.png"
+  );
+  $(".MDgameList6 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/6.png"
+  );
+});
+$(".MDgameList2 img").click(function () {
+  $(this).attr("src", "./img/桌機index_img/GAME/MDgameList/List2.png");
+  $(".MDgameList1 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/1.png"
+  );
+  $(".MDgameList3 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/3.png"
+  );
+  $(".MDgameList4 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/4.png"
+  );
+  $(".MDgameList5 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/5.png"
+  );
+  $(".MDgameList6 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/6.png"
+  );
+});
+$(".MDgameList3 img").click(function () {
+  $(this).attr("src", "./img/桌機index_img/GAME/MDgameList/List3.png");
+  $(".MDgameList1 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/1.png"
+  );
+  $(".MDgameList2 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/2.png"
+  );
+  $(".MDgameList4 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/4.png"
+  );
+  $(".MDgameList5 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/5.png"
+  );
+  $(".MDgameList6 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/6.png"
+  );
+});
+$(".MDgameList4 img").click(function () {
+  $(this).attr("src", "./img/桌機index_img/GAME/MDgameList/List4.png");
+  $(".MDgameList1 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/1.png"
+  );
+  $(".MDgameList2 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/2.png"
+  );
+  $(".MDgameList3 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/3.png"
+  );
+  $(".MDgameList5 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/5.png"
+  );
+  $(".MDgameList6 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/6.png"
+  );
+});
+$(".MDgameList5 img").click(function () {
+  $(this).attr("src", "./img/桌機index_img/GAME/MDgameList/List5.png");
+  $(".MDgameList1 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/1.png"
+  );
+  $(".MDgameList2 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/2.png"
+  );
+  $(".MDgameList3 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/3.png"
+  );
+  $(".MDgameList4 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/4.png"
+  );
+  $(".MDgameList6 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/6.png"
+  );
+});
+$(".MDgameList6 img").click(function () {
+  $(this).attr("src", "./img/桌機index_img/GAME/MDgameList/List6.png");
+  $(".MDgameList1 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/1.png"
+  );
+  $(".MDgameList2 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/2.png"
+  );
+  $(".MDgameList3 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/3.png"
+  );
+  $(".MDgameList4 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/4.png"
+  );
+  $(".MDgameList5 img").attr(
+    "src",
+    "./img/桌機index_img/GAME/MDgameList/5.png"
+  );
+});
